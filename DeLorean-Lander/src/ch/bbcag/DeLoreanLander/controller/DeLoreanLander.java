fe1e4a4b66e90564f91a23e45f18121b33328d32
zgasfl <zgasfl@WS-ZGASFL.bbcnet-zh.local>
2018-04-17 11:19:38 +0200

    Bilder hinzugefügt & File DeLoreanView bearbeitet.

--- DeLorean-Lander/src/ch/bbcag/DeLoreanLander/controller/DeLoreanLander.java
+++ DeLorean-Lander/src/ch/bbcag/DeLoreanLander/controller/DeLoreanLander.java
@@ -10,8 +10,81 @@ public class DeLoreanLander {
 	private final HiResTimer timer = new HiResTimer();
 	private double speedDown;
 	private double speedUp;
-	private boolean fuelExpired;
+	private boolean fuelExpired; // Kraftstoff abgelaufen
+	private double remainFuel; //verbleibender Kraftstoff
+	private final Actor firstLandingField = new Actor("sprites/");
+	private final Actor secondLandingField = new Actor("sprites/");
+	private final Actor thirdLandingField = new Actor("sprites/");
 	
 	public DeLoreanLander() {
+		
 		}
+
+	public double getStartFuel() {
+		return startFuel;
+	}
+
+	public void setStartFuel(double startFuel) {
+		this.startFuel = startFuel;
+	}
+
+	public Actor getLorean() {
+		return lorean;
+	}
+
+	public boolean isLanded() {
+		return isLanded;
+	}
+
+	public void setLanded(boolean isLanded) {
+		this.isLanded = isLanded;
+	}
+
+	public HiResTimer getTimer() {
+		return timer;
+	}
+
+	public double getSpeedDown() {
+		return speedDown;
+	}
+
+	public void setSpeedDown(double speedDown) {
+		this.speedDown = speedDown;
+	}
+
+	public double getSpeedUp() {
+		return speedUp;
+	}
+
+	public void setSpeedUp(double speedUp) {
+		this.speedUp = speedUp;
+	}
+
+	public boolean isFuelExpired() {
+		return fuelExpired;
+	}
+
+	public void setFuelExpired(boolean fuelExpired) {
+		this.fuelExpired = fuelExpired;
+	}
+
+	public double getRemainFuel() {
+		return remainFuel;
+	}
+
+	public void setRemainFuel(double remainFuel) {
+		this.remainFuel = remainFuel;
+	}
+
+	public Actor getFirstLandingField() {
+		return firstLandingField;
+	}
+
+	public Actor getSecondLandingField() {
+		return secondLandingField;
+	}
+
+	public Actor getThirdLandingField() {
+		return thirdLandingField;
+	}
 }
