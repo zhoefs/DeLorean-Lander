e7ce72efe9cbdc479ee0bce43f17de2c811cbe7e
zgasfl <zgasfl@WS-ZGASFL.bbcnet-zh.local>
2018-04-17 10:44:20 +0200

    Bilder und DeLoreanLander File hinzugefügt

---
+++ DeLorean-Lander/src/ch/bbcag/DeLoreanlander/controller/DeLoreanLander.java
@@ -0,0 +1,17 @@
+package ch.bbcag.DeLoreanlander.controller;
+
+import ch.aplu.jgamegrid.Actor;
+import ch.aplu.util.HiResTimer;
+
+public class DeLoreanLander {
+	private double startFuel= 1000;
+	private final Actor lorean = new Actor("sprites/lorean_car.gif");
+	private boolean isLanded = false;
+	private final HiResTimer timer = new HiResTimer();
+	private double speedDown;
+	private double speedUp;
+	private boolean fuelExpired;
+	
+	public DeLoreanLander() {
+		}
+}
