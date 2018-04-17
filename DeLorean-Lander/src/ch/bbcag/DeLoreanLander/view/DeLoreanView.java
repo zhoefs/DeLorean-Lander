0eced7d3f42a0d024c11dfb1bd0bd7d03fb8c3f6
zgasfl <zgasfl@WS-ZGASFL.bbcnet-zh.local>
2018-04-17 12:55:02 +0200

    Altes Hintergrundbild umbenannt & DeLoreanView File ergänzt

--- DeLorean-Lander/src/ch/bbcag/DeLoreanLander/view/DeLoreanView.java
+++ DeLorean-Lander/src/ch/bbcag/DeLoreanLander/view/DeLoreanView.java
@@ -4,20 +4,21 @@ import ch.aplu.jgamegrid.GameGrid;
 import ch.aplu.jgamegrid.Location;
 import ch.bbcag.DeLoreanLander.controller.DeLoreanLander;
 
-public class DeLoreanView extends GameGrid{
+public class DeLoreanView extends GameGrid {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	public DeLoreanView() {
-		super(1700, 100, 1, null, "resources/sprites/lander_background.gif", false);
+		super(1700, 100, 1, null, "resources/sprites/western_background.gif", false);
 		setSimulationPeriod(50);
 		final DeLoreanLander lander = new DeLoreanLander();
-//		addActor(lander, new Location(330, 100));
-//		addKeyListener(lander);
+		// addActor(lander, new Location(330, 100));
+		// addKeyListener(lander);
 		show();
 		GameGrid.delay(3000);
 		doRun();
 	}
+
 	public static void main(final String[] args) {
 		new DeLoreanView();
 	}
