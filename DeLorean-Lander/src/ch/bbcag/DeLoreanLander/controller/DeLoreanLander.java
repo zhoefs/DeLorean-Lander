24752626425ae13cfe215e45ebc378d938cef37b
zgasfl <zgasfl@WS-ZGASFL.bbcnet-zh.local>
2018-05-22 14:45:21 +0200

    Changed the factor for the score

--- DeLorean-Lander/src/ch/bbcag/DeLoreanLander/controller/DeLoreanLander.java
+++ DeLorean-Lander/src/ch/bbcag/DeLoreanLander/controller/DeLoreanLander.java
@@ -58,7 +58,7 @@ public class DeLoreanLander extends Actor implements GGActorCollisionListener {
 			gameGrid.addActor(firework, new Location(900, 800));
 			gameGrid.addActor(new TextActor("Press START to Restart or BACK to Leave", Color.WHITE, Color.BLACK,
 					new Font(Font.SANS_SERIF, Font.BOLD, 24)), new Location(700, 400));
-			score = (int) (remainFuel * 5 + passedTime);
+			score = (int) (remainFuel * 5 + passedTime * 50);
 			gameGrid.getBg().drawText("You've reached " + score + " " + "Points", new Point(780, 500));
 			gameGrid.doRun();
 
