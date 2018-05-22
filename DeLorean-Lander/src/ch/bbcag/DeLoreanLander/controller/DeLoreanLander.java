9fb9397fceb1617623a684ac422afdd150b61406
zhoefs <zhoefs@WS-ZHOEFS.bbcnet-zh.local>
2018-05-22 14:50:33 +0200

    score doesnt go under 0

--- DeLorean-Lander/src/ch/bbcag/DeLoreanLander/controller/DeLoreanLander.java
+++ DeLorean-Lander/src/ch/bbcag/DeLoreanLander/controller/DeLoreanLander.java
@@ -59,6 +59,9 @@ public class DeLoreanLander extends Actor implements GGActorCollisionListener {
 			gameGrid.addActor(new TextActor("Press START to Restart or BACK to Leave", Color.WHITE, Color.BLACK,
 					new Font(Font.SANS_SERIF, Font.BOLD, 24)), new Location(700, 400));
 			score = (int) (remainFuel * 5 + passedTime * 50);
+			if (score <= 0 ) {
+				score = 0;
+			}
 			gameGrid.getBg().drawText("You've reached " + score + " " + "Points", new Point(780, 500));
 			gameGrid.doRun();
 
